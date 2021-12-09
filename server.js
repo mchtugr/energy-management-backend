6c7a9642bc30ff83541c85d59a7e01ac8273a129
murtazaaylak <murtazaaylak@gmail.com>
2021-12-09 23:28:51 +0300

    feat: setup server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const connectDB = require('./config/db')
+const cors = require('cors')
+
+const app = express()
+
+// connect Database
+connectDB()
+
+// init middlewares
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// define routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`)
+})
