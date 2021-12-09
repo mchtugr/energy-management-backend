86da9c130fc44c74a00f52364fa9c70025def6bc
murtazaaylak <murtazaaylak@gmail.com>
2021-12-09 13:35:25 +0300

    feat(userModel): create mongoose model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'user'],
+  },
+})
+
+module.exports = mongoose.model('user', UserSchema)
