6baf9397766a5d9e6ad7038b60246c7654302b09
murtazaaylak <murtazaaylak@gmail.com>
2021-12-11 16:20:44 +0300

    chore: change mongodb config module name

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () => {
+const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
@@ -15,4 +15,4 @@ const connectDB = async () => {
   }
 }
 
-module.exports = connectDB
+module.exports = connectMongoDB
