5400b0a82d939f98f3cba30c01493fc654743bf4
murtazaaylak <murtazaaylak@gmail.com>
2021-12-11 21:34:42 +0300

    feat: update factory list

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,7 +1,7 @@
 const client = require('../config/pg')
 
 // @desc retrieve factory list
-// @route POST /api/dashboard/factories
+// @route GET /api/dashboard/factories
 // @access private
 
 const retrieveAllFactories = (req, res) => {
@@ -11,8 +11,29 @@ const retrieveAllFactories = (req, res) => {
       res.status(200).json({ data: response.rows })
     })
     .catch((err) => {
-      res.status(400).json({})
+      res.status(400).json({ err: err.message })
     })
 }
 
-module.exports = { retrieveAllFactories }
+// @desc UPDATE factory list
+// @route POST /api/dashboard/factories
+// @access private
+
+const updateFactoryList = (req, res) => {
+  const { id } = req.params
+  const { name, membership_date, membership_due, company_size, gold_member } =
+    req.body
+  client
+    .query(
+      'UPDATE factories SET name=$1,  membership_date=$2,  membership_due=$3,  company_size=$4, gold_member=$5 WHERE id = $6 RETURNING *',
+      [name, membership_date, membership_due, company_size, gold_member, id]
+    )
+    .then((response) => {
+      res.status(200).json({ data: response.rows })
+    })
+    .catch((err) => {
+      res.status(400).json({ err: err })
+    })
+}
+
+module.exports = { retrieveAllFactories, updateFactoryList }
